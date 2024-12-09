f4e88248fcacfe018819f6df16d369017a9412c3
Paulo Ricardo De Oliveira Martins <ht3012158@hto.ifsp.edu.br>
2024-12-09 20:19:55 -0300

    Atualização no schema da tabela de alugueis

--- banco.sql
+++ banco.sql
@@ -18,11 +18,15 @@ CREATE TABLE espacos (
 CREATE TABLE alugueis (
     aluguel_id INT PRIMARY KEY AUTO_INCREMENT,
     usuario_id INT NOT NULL,
+    nome_usuario VARCHAR(100) NOT NULL,
     espaco_id INT NOT NULL,
     valor_final DECIMAL(10, 2),
     metodo_pagamento VARCHAR(50),
     periodo_inicio DATETIME NOT NULL,
     periodo_fim DATETIME NOT NULL,
+    status_pagamento ENUM('pendente', 'pago', 'cancelado') DEFAULT 'pendente',
+    status ENUM('ativo', 'inativo') DEFAULT 'ativo',
+    ocorrencia VARCHAR(255),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
     FOREIGN KEY (espaco_id) REFERENCES espacos(espaco_id)
 );
